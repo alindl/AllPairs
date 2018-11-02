afb4bdab1868f8c49e4e74f416fa8384ce9668a8
Andreas Lindlbauer <andreas.lindlbauer@stud.sbg.ac.at>
2018-11-02 20:59:03 +0100

    Named variables as in assignment, implemented a bit of algorithm

--- AllPairs.py
+++ AllPairs.py
@@ -6,16 +6,26 @@ parser.add_argument('input_file', default='bms-pos.txt')
 parser.add_argument('jaccard_threshold', default=0.5, type=float)
 args = parser.parse_args()
 
+with open(args.input_file) as input_file:  
+    full_file = input_file.read()
+    reading_time = time.process_time()
+    sum_results = 0
+    # I implemented as array with tuple and integer
+    for line in full_file:
+        M_dict = {} # Key: candidate, Value: number of intersecting tokens found so far
+        sum_results += 1 
+        
+    
+
+        # Verify
+
 # print(args.input_file)
 # print(args.jaccard_threshold)
 
-reading_time = time.process_time()
 
 
-
-
-cpu_time = time.process_time() - reading_time
-sum_results = 1
+join_time_in_seconds = time.process_time() - reading_time
+output_size = 1
 print(sum_results)
-print(cpu_time)
+print(join_time_in_seconds)
 
